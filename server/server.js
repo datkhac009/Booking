2524411256e50deb8a1aa776cc39382ec78c5889
Sunwoo Jun <37898596+whitersun@users.noreply.github.com>
2023-09-13 00:36:32 +0800

    updated server.js

--- server/server.js
+++ server/server.js
@@ -1,22 +1,24 @@
-const express = require('express')
-    const app = express()
-    const port = 3000
 
-    app.get('/', (req, res) => {
-      res.send('Hello World My Project 123')
-    })
+// import express from 'express';
 
-    app.listen(port, () => {
-      console.log(`Hello Example app listening on port ${port}`)
-    })
-// import express from "express";
-// const app = express();
-// const PORT = 5000;
+// const app = express()
+// const port = process.env.PORT || 3000
 
-// app.use(express.json())
-// app.get('/',(res,req) => {
-//     res.send('Hello Word')
+// app.get('/', (req, res) => {
+//   res.send('Hello World My Project 123')
 // })
-// app.listen(PORT,() => {
-//     console.log(`Connect to ${PORT}`);
+
+// app.listen(port, () => {
+//   console.log(`Hello Example app listening on port ${port}`)
 // })
+import express from "express";
+const app = express();
+const PORT = 5000;
+
+app.use(express.json())
+app.get('/',(req, res) => {
+    res.send('Hello Word')
+})
+app.listen(PORT,() => {
+    console.log(`Connect to ${PORT}`);
+})
